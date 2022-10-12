c81eb86f6de80f675ffc2680a13378fb1e754b22
62010914602 <99231147+62010914602@users.noreply.github.com>
2022-10-12 10:18:42 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Thitipong Cheujunthuek</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="w3-black w3-text-white w3-card-4">
+        <div class="w3-display-container">
+          <img src="C:\Users\Administrator\Desktop\Cap.png" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-black">
+           
+          </div>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-person fa-fw w3-margin-right w3-large w3-text-red"></i><b>Thitipong Cheujuntheuk</b></p>
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-green"></i>Student</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-yellow"></i>Maha Sarakham, Thailand</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-purple"></i>62010914602	@msu.ac.th</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-red"></i>0880326022</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-green"></i>Skills</b></p>
+          <p>Microsoft Excel</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:80%">80%</div>
+          </div>
+          <p>Microsoft Word</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:60%">
+              <div class="w3-center w3-text-white">60%</div>
+            </div>
+          </div>
+          <p>Python</p>
+          <div class="w3-light-grey w3-round-xlarge w3-small">
+            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:30%">30%</div>
+          </div>
+          <br>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-blue"></i>Languages</b></p>
+                     <p>Thai</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-center w3-red" style="height:24px;width:90%">90%</div>
+          </div>
+          <p>English</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-center w3-red" style="height:24px;width:80%">80%</div>
+          </div>
+          <p>Chinese</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-center w3-red" style="height:24px;width:20%">20%</div>
+          </div>
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-gray w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-orange"></i>Education</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Business Information Technology</b></h5>
+          <p><b>Mahasarakham University</b>          </p>
+          <p>2019-Present</p>
+          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw w3-margin-right"></i>First-year</h6>
+          <p>Introduction to Business</p>
+          <p>Principles of Marketing</p>
+          <p>Business Computer and Information Technology</p>
+          <p>Introduction to Computer Programming in Business</p>
+          <p>Principles of Accounting</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Second-year</h6>
+          <p>Business Law</p>
+          <p>Computer Technology and Operating System</p>
+          <p>Data Structure and Algorithms</p>
+          <p>Analysis and Design for Business Information System</p>
+          <p>Business Database Management System</p>
+          <p>Business Programming</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw w3-margin-right"></i><b>Third-year</b></h6>
+          <p>Production and Operations Management</p>
+          <p>Internet Marketing</p>
+          <p>Business Data Communication and Computer Network</p>
+          <p>Business Statistics</p>
+          <p>English for Business<br>
+          </p>
+        </div>
+
+      <div class="w3-container">
+          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw w3-margin-right"></i><b>Four-year</b></h6>
+          <p>Business Information Technology Management</p>
+          <p>Big Data Analytics</p>
+          <p>Mobile Programming and Wireless Components in Business</p>
+          <p>Applications of Enterprise Resource Planning Software</p>
+          <p>User Experience/User Interface Design and Development</p>
+          <p>Business Policy and Strategic Management</p>
+          <p><br>
+        </p>
+      </div>
+    </div>
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-arrow-up fa-fw w3-margin-right w3-xxlarge w3-text-red"></i>Self-Development</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>การสร้างสรรค์เนื้อหาด้วยข้อมูล (Data) เพื่อเพิ่มคุณค่าขององค์กร </b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Thaimooc</h6>        
+          <hr>
+        </div>
+        
+         <div class="w3-container">
+          <h5 class="w3-opacity"><b>การจัดการธุรกิจการค้าสมัยใหม่ในยุค Thailand 4.0</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Thaimooc</h6>      
+          <hr>
+        </div>
+        
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>เทคโนโลยีการเงิน</b></h5>
+          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Thaimooc</h6>       
+          <hr>
+        </div>
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-green w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p>Powered by <a href="http://mbs.msu.ac.th/2022/" target="_blank">MBS,MSU</a></p>
+</footer>
+
+</body>
+</html>
