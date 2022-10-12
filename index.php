3ccfb0ce7f413f5205982a62aefa96da2db3fe5c
62010914602 <99231147+62010914602@users.noreply.github.com>
2022-10-12 10:58:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
     
       <div class="w3-black w3-text-white w3-card-4">
         <div class="w3-display-container">
-          <img src="123.jpg" style="width:100%;" alt="Avatar">
+          <img src="123.jpg" style="width:100%;" alt="pictures">
           <div class="w3-display-bottomleft w3-container w3-text-black">
            
           </div>
